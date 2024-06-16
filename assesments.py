5be2c0e2de8bd0beb3f7ab3501c563df69aa25fe
LukeLukeRW <151480620+LukeLukeRW@users.noreply.github.com>
2024-06-16 19:53:25 +1000

    Update assesments.py
    made the code cleaner idk what gibberish there was before lul

--- assesments.py
+++ assesments.py
@@ -1,29 +1,22 @@
-import selenium ;from bs4 import BeautifulSoup;import time;from selenium.webdriver.chrome.service import Service as ChromeService;from webdriver_manager.chrome import ChromeDriverManager;import os;import json;import keyboard;from selenium.webdriver.chrome.options import Options;import pyautogui
-driver = selenium.webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+import selenium
+from bs4 import BeautifulSoup
+import time
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+import json;import keyboard
+import pyautogui
 
-url = "url goes here :)))))) shhhhhhhhhhhh, confidential"
+def main(i,txtsubject):
+    for i in range(1,6):
 
-#opens url with head
-driver.get(url)
-
-txtsubject = []
-one_time = True
-
-def main(times_done):
-    
-    global one_time,txtsubject
-    for k in range(times_done):
-
-        if one_time == True:
-            wait_for_psw_input()
-            one_time = False
+        if i == 1:
+            psw_input()
 
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         days = soup.find_all('div',class_="fc-daygrid-day-frame fc-scrollgrid-sync-inner")
         for i in days:
             try:
                 txtsubject_dic = {}
-                temp_list = []
                 subject = i.find_all(class_="fc-event-title")
 
                 for j in subject:
@@ -33,34 +26,33 @@ def main(times_done):
                     else:
                         pass
 
-                if 'subject' in txtsubject_dic:  # Check if subject exists
-                    
+                if 'subject' in txtsubject_dic: #if subject exists...
                     txtsubject_dic['date'] = (i.find('a', class_='fc-daygrid-day-number')['aria-label'])
-
                     txtsubject.append(txtsubject_dic)
 
             except ValueError:
                 pass
+    return txtsubject
 
-
-        pyautogui.click(737,618) #next
-        time.sleep(3) # load new webpage
-
-def wait_for_psw_input():
-    time.sleep(1) #load webpage
+def psw_input():
+    time.sleep(1)
     pyautogui.typewrite("USERNAME GOES HERE");keyboard.press('tab');keyboard.write("password goes here :)");keyboard.press_and_release('enter')
-    time.sleep(5) #load wp
+    time.sleep(5)
+
+def click_next_button():
+    pyautogui.click(737,618)
+    time.sleep(3)
+
+def saving(txtsubject):
+    with open ('Assessments.json','w') as json_file:
+        json.dump(txtsubject,json_file,indent=4)
+
 
 if __name__ == '__main__':
-    main(5) # how many iterations
 
-#append to a csv
-with open ('Assessments.json','w') as json_file:
-    json.dump(txtsubject,json_file,indent=4)
+    txtsubject = []
+    url = "url goes here :)"
+    driver = selenium.webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver.get(url)
 
-'''
-<div class="fc-daygrid-day-frame fc-scrollgrid-sync-inner"><div class="fc-daygrid-day-top"><a id="fc-dom-806" class="fc-daygrid-day-number" aria-label="February 21, 2024">21</a></div><div class="fc-daygrid-day-events"><div class="fc-daygrid-event-harness" style="margin-top: 0px;"><a class="fc-daygrid-event fc-daygrid-block-event fc-h-event fc-event fc-event-start fc-event-end fc-event-future source personal true" tabindex="0"><div class="fc-event-main"><span data-v-app=""><div data-v-054bf15a="" class="event-container"><b data-v-054bf15a="" class="fc-event-title">3:35pm</b> <!--v-if--> <span data-v-054bf15a="" class="fc-event-title">VCE Units 3 &amp; 4 Physics (PHY-12-E1,PHY-12-E2)<br>Summative AT 3.1: Problem solving in real world contexts (Motion in 2D)</span></div></span></div></a></div><div class="fc-daygrid-day-bottom" style="margin-top: 0px;"></div></div><div class="fc-daygrid-day-bg"></div></div>
- class="fc-event-title">3:35pm</b> <!--v-if--> <span data-v-054bf15a="" class="fc-event-title">VCE Units 3 &amp; 4 Physics (PHY-12-E1,PHY-12-E2)<br>Summative AT 3.1: Problem solving in real world contexts (Motion in 2D)</span></div></span></div></a></div><div class="fc-daygrid-day-bottom" style="margin-top: 0px;"></div></div><div class="fc-daygrid-day-bg"></div></div>
-
-'''
-
+    saving(main(txtsubject) )
